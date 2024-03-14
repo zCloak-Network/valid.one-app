b8b7a9b597f61bae66ccff4ece2e7e53617f910f
tower1229 <tower1229@foxmail.com>
2024-03-14 20:45:24 +0800

    style: sign result

--- app/sign/signer/SignatureResult.tsx
+++ app/sign/signer/SignatureResult.tsx
@@ -21,14 +21,14 @@ export default function SignatureResult(props: {
       onClose={props.onClose}
     >
       <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-3">
-          <IconSign className="relative h-[55px] w-[55px]" />
+        <div className="flex gap-3 items-center">
+          <IconSign className="h-[55px] w-[55px] relative" />
 
-          <div className="flex flex-1 flex-col items-start gap-2">
-            <span className="text-sm font-medium leading-normal  tracking-tight text-black">
+          <div className="flex flex-col flex-1 gap-2 items-start">
+            <span className="font-medium text-sm text-black  leading-normal tracking-tight">
               The message has been signed at
             </span>
-            <div className="flex items-center gap-1 text-xs font-semibold leading-none text-neutral-400">
+            <div className="flex font-semibold text-xs leading-none text-neutral-400 gap-1 items-center">
               <IconCloak className="h-4 w-4" />
               2024-12-12: 00:12:12
             </div>
@@ -38,7 +38,33 @@ export default function SignatureResult(props: {
           <TextareaWithCopy value={"message"} />
         </label>
         <div className="border-t"></div>
-        share link
+
+        <div className="flex px-20 gap-5 justify-between">
+          <div className="flex flex-col flex-1">
+            <div className="flex m-auto bg-gray-800 rounded-[1000px] h-[57px] px-4 w-[57px] items-center justify-center">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/c830d601174db9e130799b095f4f0d7d4f0cae14e7dcb9b377e925a4735e9f94?apiKey=bdc70480199b4c7f9edcac5b83339cc0&"
+                className="w-6 aspect-square"
+              />
+            </div>
+            <div className="font-semibold mt-3 text-center text-xs text-slate-900 leading-4 whitespace-nowrap">
+              Share link
+            </div>
+          </div>
+          <div className="flex flex-col flex-1">
+            <div className="flex m-auto bg-blue-700 rounded-[1000px] h-14 px-4 w-14 items-center justify-center">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e88297bcb3b2a363e6583446b75f604789f5d3602708c34b14f96931c78caf48?apiKey=bdc70480199b4c7f9edcac5b83339cc0&"
+                className="w-6 aspect-square"
+              />
+            </div>
+            <div className="font-semibold mt-3 text-center text-xs text-slate-900 leading-4 whitespace-nowrap">
+              QR Code
+            </div>
+          </div>
+        </div>
       </div>
     </ActionModal>
   );
--- app/sign/signer/page.tsx
+++ app/sign/signer/page.tsx
@@ -20,20 +20,20 @@ export default function Signer() {
 
   return (
     <div className="rounded-xl bg-[#F9FAFB] p-4">
-      <div className="mb-4 flex h-[52px] items-center gap-2 rounded-xl border border-zinc-200 p-4">
-        <div className="relative h-6 w-6">
-          <div className="absolute left-[1px] top-[1px] h-[22px] w-[22px] rounded-full bg-zinc-300" />
+      <div className="border rounded-xl flex border-zinc-200 h-[52px] mb-4 p-4 gap-2 items-center">
+        <div className="h-6 w-6 relative">
+          <div className="rounded-full bg-zinc-300 h-[22px] top-[1px] left-[1px] w-[22px] absolute" />
         </div>
-        <div className="text-sm font-medium text-gray-900">1124731</div>
+        <div className="font-medium text-sm text-gray-900">1124731</div>
       </div>
 
-      <div className="mb-4 flex flex-col gap-4">
-        <div className=" self-stretch text-sm  text-zinc-500">
+      <div className="flex flex-col mb-4 gap-4">
+        <div className=" text-sm text-zinc-500  self-stretch">
           What do you want to sign?
         </div>
         <div className="flex w-full items-center">
           {signTypes.map((signType) => (
-            <label className="label flex-1 cursor-pointer justify-normal gap-2">
+            <label className="cursor-pointer flex-1 gap-2 label justify-normal">
               <input
                 type="radio"
                 name="radio-10"
@@ -50,30 +50,35 @@ export default function Signer() {
       <div className="mb-4">
         {type === "message" && (
           <textarea
-            className="textarea-border textarea w-full"
+            className="w-full textarea-border textarea"
             placeholder="Please enter your message here"
           ></textarea>
         )}
         {type === "file" && (
-          <input type="file" className="file-input w-full max-w-xs" />
+          <div className="min-h-40 form-control">
+            <label className="cursor-pointer gap-2 label"></label>
+            <input type="file" className="max-w-xs w-full file-input" />
+          </div>
         )}
       </div>
 
-      <div className="form-control">
-        <label className="label cursor-pointer gap-2">
-          <input
-            type="checkbox"
-            defaultChecked
-            className="checkbox-primary checkbox"
-          />
-          <span className="label-text text-xs">
-            Select this and we'll create a shareable link for your signed
-            message. Note: we'll store your message.
-          </span>
-        </label>
-      </div>
+      {type === "message" && (
+        <div className="form-control">
+          <label className="cursor-pointer gap-2 label">
+            <input
+              type="checkbox"
+              defaultChecked
+              className="checkbox-primary checkbox"
+            />
+            <span className="text-xs label-text">
+              Select this and we'll create a shareable link for your signed
+              message. Note: we'll store your message.
+            </span>
+          </label>
+        </div>
+      )}
 
-      <div className="my-4 border"></div>
+      <div className="border my-4"></div>
 
       <button
         className="btn btn-neutral btn-block"
