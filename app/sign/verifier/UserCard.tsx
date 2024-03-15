cf688777d9e9b2528a36156e6bd4946ad517aed5
refined-x <tower1229@foxmail.com>
2024-03-15 14:32:02 +0800

    style: user card

--- app/sign/verifier/UserCard.tsx
+++ app/sign/verifier/UserCard.tsx
@@ -5,41 +5,31 @@ export default function UserCard(props: { validId: string }) {
   const [openResult, setOpenResult] = useState(false);
 
   return (
-    <div className="flex h-32 flex-col items-start justify-center gap-4 rounded-xl bg-gray-800 p-4">
-      <div className="flex items-center">
+    <div className="flex flex-col gap-4 rounded-xl bg-gray-800 p-4">
+      <div className="flex w-full items-center">
         <div className="flex-1 text-sm font-medium leading-tight tracking-tight text-white">
           Valid ID
         </div>
         <div className=" text-sm font-medium text-zinc-300">121213</div>
       </div>
-      <div className="relative h-[60px] w-[174.87px]">
-        <div className="absolute left-0 top-0 h-[60px] w-[60px]">
-          <div className="absolute left-0 top-0 h-[60px] w-[60px] rounded-full border border-neutral-400" />
-          <div className="absolute left-[3px] top-[3px] h-[54px] w-[54px] rounded-[27px] bg-zinc-300" />
+      <div className="flex items-center gap-2">
+        <div className="h-[60px] w-[60px] rounded-full border border-neutral-400 p-[2px]">
           <img
-            className="absolute left-[-33.65px] top-[-30px] h-[146.39px] w-[129.65px]"
+            className="h-full w-full rounded-full object-cover"
             src="https://via.placeholder.com/130x146"
           />
         </div>
-        <div className="absolute left-[70.87px] top-[33px] flex h-5 w-[104px] items-start justify-start gap-2">
-          <div className="relative h-5 w-5">
-            <div className="absolute left-0 top-0 h-5 w-5 rounded-[15px] bg-gray-600" />
-            <div className="absolute left-[4.58px] top-[4.58px] h-[10.83px] w-[11.25px]" />
+        <div className="flex flex-1 flex-col gap-2">
+          <div className="w-full text-[15px] font-bold text-white">
+            John Doe
           </div>
-          <div className="relative h-5 w-5">
-            <div className="absolute left-[4.58px] top-[4.58px] h-[10.83px] w-[11.25px]" />
+
+          <div className="flex w-full gap-2">
+            <div className="relative h-5 w-5 rounded-[15px] bg-gray-600"></div>
+            <div className="relative h-5 w-5 rounded-[15px] bg-gray-600"></div>
+            <div className="relative h-5 w-5 rounded-[15px] bg-gray-600"></div>
+            <div className="relative h-5 w-5 rounded-[15px] bg-gray-600"></div>
           </div>
-          <div className="relative h-5 w-5">
-            <div className="absolute left-0 top-0 h-5 w-5 rounded-[15px] bg-gray-600" />
-            <div className="absolute left-[4.58px] top-[4.58px] h-[10.83px] w-[11.25px]" />
-          </div>
-          <div className="relative h-5 w-5">
-            <div className="absolute left-0 top-0 h-5 w-5 rounded-[15px] bg-gray-600" />
-            <div className="absolute left-[4.58px] top-[4.58px] h-[10.83px] w-[11.25px]" />
-          </div>
-        </div>
-        <div className="absolute left-[70px] top-[1px] w-[104.65px] font-['Manrope'] text-[15px] font-bold text-white">
-          John Doe
         </div>
       </div>
     </div>
