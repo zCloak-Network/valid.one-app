8e91859b2053d12f2696b9604be03fb062a08624
refined-x <tower1229@foxmail.com>
2024-03-20 10:58:50 +0800

    style: fit large screen

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,9 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_VALID_ID_URL: string;
   readonly VITE_APP_CANISTER_ID: string;
   readonly VITE_APP_CANISTER_HOST: string;
-  readonly VITE_APP_CANISTER_ID_VALID_ONE_BACKEND: string;
-  readonly VITE_APP_VALID_ONE_BACKEND_CANISTER_ID: string;
-  readonly VITE_APP_DFX_NETWORK: string;
 }
 
 interface ImportMeta {
