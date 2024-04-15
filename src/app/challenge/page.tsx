8cddda529f0f860e54a1e9dcf39de6de2e4e3706
refined-x <tower1229@foxmail.com>
2024-04-15 15:07:29 +0800

    fix: challenge tip

--- src/app/challenge/page.tsx
+++ src/app/challenge/page.tsx
@@ -10,7 +10,7 @@ import {
 import Logo from "@/assets/landing-page/logo.png";
 import Alpha from "@/assets/landing-page/alpha.png";
 import { sha256OfString } from "@/utils";
-import { useToast } from "@/components";
+import { useToast, ToastProvider } from "@/components";
 import initActor, { actor } from "@/utils/canister";
 import type { challengeData } from "@/types";
 import { getChallenge, sendChallenge } from "@/api";
@@ -58,7 +58,7 @@ export default observer(function ChallengePage() {
   const [challengeData, setChallengeData] = useState<
     challengeData | undefined
   >();
-  const [challengeSuccess, setChallengeSuccess] = useState(false);
+  const [challengeStatus, setChallengeStatus] = useState<0 | 1 | string>(0);
 
   const messageSHA256 = useMemo(() => {
     return challengeData?.challenge
@@ -107,11 +107,12 @@ export default observer(function ChallengePage() {
           signature: (res as any)["Ok"]?.signature,
         });
 
-        if (challengeRes.data) {
-          setChallengeSuccess(true);
+        if (challengeRes.code === 200) {
+          setChallengeStatus(1);
         } else {
-          toast &&
-            toast({ type: "error", message: "Verify fail, please try again" });
+          setChallengeStatus(
+            challengeRes.msg || "Verify fail, please try again"
+          );
         }
       } else {
         toast && toast({ type: "error", message: "sign fail" });
@@ -140,70 +141,82 @@ export default observer(function ChallengePage() {
   };
 
   return (
-    <div className="h-[100vh] pt-20 flex flex-col items-center justify-center">
-      <header className="absolute left-0 w-full top-0 h-20 flex items-center px-4 bg-[#fafbff] z-10 shadow-lg">
-        <img src={Logo} alt="logo" className="h-[28px] w-[28px]" />
-        <img src={Alpha} alt="alpha" className="h-[24px] ml-2" />
-        <div className="flex-1"></div>
-        <button className="btn ]">Valid ID:{User.id}</button>
-      </header>
-      {!initLoading ? (
-        challengeData ? (
-          <div className="rounded-lg bg-white shadow-2xl w-[60%] min-w-[300px] max-w-[640px]">
-            <div className="border-b px-8 py-4 font-semibold">
-              Valid One Telegram Challenge
-            </div>
-            <div className="p-8">
-              {/* challengeID:{challengeID} */}
-              {challengeData.status === 0 && (
-                <>
-                  <div className="mb-8">
-                    Your Telegram friend
-                    <kbd className="kbd mx-1">
-                      {challengeData.requestUser.name}
-                    </kbd>
-                    requests verification of your identity.
-                  </div>
-                  <div className="text-center">
-                    {challengeSuccess ? (
-                      <div className="flex items-center justify-center gap-2">
-                        <a
-                          href={import.meta.env.VITE_APP_TELEGRAM_BOT_URL}
-                          className="btn btn-sm btn-neutral bg-[#000000]"
+    <ToastProvider>
+      <div className="h-[100vh] pt-20 flex flex-col items-center justify-center">
+        <header className="absolute left-0 w-full top-0 h-20 flex items-center px-4 bg-[#fafbff] z-10 shadow-lg">
+          <img src={Logo} alt="logo" className="h-[28px] w-[28px]" />
+          <img src={Alpha} alt="alpha" className="h-[24px] ml-2" />
+          <div className="flex-1"></div>
+          <button className="btn ]">Valid ID:{User.id}</button>
+        </header>
+        {!initLoading ? (
+          challengeData ? (
+            <div className="rounded-lg bg-white shadow-2xl w-[60%] min-w-[300px] max-w-[640px]">
+              <div className="border-b px-8 py-4 font-semibold">
+                Valid One Telegram Challenge
+              </div>
+              <div className="p-8">
+                {/* challengeID:{challengeID} */}
+                {challengeData.status === 0 && (
+                  <>
+                    <div className="mb-8">
+                      Your Telegram friend
+                      <kbd className="kbd mx-1">
+                        {challengeData.requestUser.name}
+                      </kbd>
+                      requests verification of your identity.
+                    </div>
+                    <div className="text-center">
+                      {challengeStatus === 1 && (
+                        <div className="flex items-center justify-center gap-2">
+                          <a
+                            href={import.meta.env.VITE_APP_TELEGRAM_BOT_URL}
+                            className="btn btn-sm btn-neutral bg-[#000000]"
+                          >
+                            Back to telegram
+                          </a>
+                          <Link className="btn btn-sm btn-link" to={"/"}>
+                            Valid One
+                          </Link>
+                        </div>
+                      )}
+
+                      {challengeStatus === 0 && (
+                        <button
+                          className="btn btn-neutral bg-[#000000]"
+                          disabled={!contIsReady()}
+                          onClick={handleUserConfirm}
                         >
-                          Back to telegram
-                        </a>
-                        <Link className="btn btn-sm btn-link" to={"/"}>
-                          Valid One
-                        </Link>
-                      </div>
-                    ) : (
-                      <button
-                        className="btn btn-neutral bg-[#000000]"
-                        disabled={!contIsReady()}
-                        onClick={handleUserConfirm}
-                      >
-                        {loading && (
-                          <span className="loading loading-spinner"></span>
-                        )}
-                        Verify
-                      </button>
-                    )}
-                  </div>
-                </>
-              )}
+                          {loading && (
+                            <span className="loading loading-spinner"></span>
+                          )}
+                          Verify
+                        </button>
+                      )}
 
-              {challengeData.status === 1 && <div>Verification completed</div>}
+                      {challengeStatus !== 0 && challengeStatus !== 1 && (
+                        <div className="alert alert-error shadow-lg">
+                          {challengeStatus}
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
 
-              {challengeData.status === 2 && <div>Verification expired</div>}
+                {challengeData.status === 1 && (
+                  <div>Verification completed</div>
+                )}
+
+                {challengeData.status === 2 && <div>Verification expired</div>}
+              </div>
             </div>
-          </div>
+          ) : (
+            "Data does not exist"
+          )
         ) : (
-          "Data does not exist"
-        )
-      ) : (
-        "Loading"
-      )}
-    </div>
+          "Loading"
+        )}
+      </div>
+    </ToastProvider>
   );
 });
