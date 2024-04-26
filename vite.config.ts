386d8c1106ee2305482c78211ecc358d1d7576bc
refined-x <tower1229@foxmail.com>
2024-04-26 10:00:59 +0800

    feat: test pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,40 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills()],
+  plugins: [
+    react(),
+    svgr(),
+    nodePolyfills(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Valid One PWA",
+        short_name: "Valid.One",
+        description: "An easy-onboard, secure identity management solution",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0",
   },
