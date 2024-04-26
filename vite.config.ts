5b852863e012be92d168f6930cdad092fad771b7
refined-x <tower1229@foxmail.com>
2024-04-26 10:55:51 +0800

    fix: image path

--- vite.config.ts
+++ vite.config.ts
@@ -35,23 +35,23 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/images/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/images/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-maskable-192x192.png",
+            src: "/images/pwa-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "pwa-maskable-512x512.png",
+            src: "/images/pwa-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
