285323739d767424dc541fc4392dd55d7069bbba
refined-x <tower1229@foxmail.com>
2024-04-26 10:35:08 +0800

    fix: update pwa

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,21 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "pwa-192x192.png",
+        "pwa-512x512.png",
+        "pwa-maskable-192x192.png",
+        "pwa-maskable-512x512.png",
+      ],
       manifest: {
         name: "Valid One PWA",
         short_name: "Valid.One",
         description: "An easy-onboard, secure identity management solution",
         theme_color: "#ffffff",
+        start_url: "./?utm_source=web_app_manifest",
+        display: "standalone",
         icons: [
           {
             src: "pwa-192x192.png",
@@ -32,6 +41,18 @@ export default defineConfig({
             src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+          },
+          {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
